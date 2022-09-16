6eba4bcc809954174cba365f2e66c9b4b6dba2cb
Mohammad Nur Hasan <hm.nur@mitani.co.jp>
2022-09-16 14:44:41 +0900

    init commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<!-- <script setup>
+import members from './data/members.js';
+</script> -->
+<template>
+  <div>
+    <div>
+      <img
+        src="https://robohash.org/d431dfed5be7a8869aa301622e90796e?set=set4&bgset=&size=400x400"
+        alt="Avatar"
+      />
+      <div>
+        <div>
+          <p>鈴木太郎</p>
+          <p>ソフトウェアエンジニア</p>
+        </div>
+        <button>メッセージ</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<!-- <div class="space-y-4">
+    <div
+      v-for="member in members"
+      class="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6"
+    >
+      <img
+        class="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+        :src="member.avatar"
+        alt="Woman's Face"
+      />
+      <div class="text-center space-y-2 sm:text-left">
+        <div class="space-y-0.5">
+          <p class="text-lg text-black font-semibold">{{ member.name }}</p>
+          <p class="text-slate-500 font-medium">{{ member.position }}</p>
+        </div>
+        <button
+          class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+        >
+          Message
+        </button>
+      </div>
+    </div>
+  </div> -->
---
+++ src/data/members.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    name: 'Ishino',
+    email: 'ishino@kokoro.com',
+    position: 'Project Manager',
+	avatar: 'https://gravatar.com/avatar/9cc3ada42e5586c101a3364368807174?s=400&d=robohash&r=x',
+  },
+  {
+    id: 2,
+    name: 'Takada',
+    email: 'takada@kokoro.com',
+    position: 'Software Engineer',
+	avatar: 'https://gravatar.com/avatar/da5df31561ac60d7e7f9dedaf1f24768?s=400&d=robohash&r=x',
+  },
+  {
+    id: 3,
+    name: 'Hasan',
+    email: 'hasan@kokoro.com',
+    position: 'Software Engineer',
+	avatar: 'https://gravatar.com/avatar/da5df31561ac60d7e7f9dedaf1f24768?s=400&d=robohash&r=x',
+  },
+];
